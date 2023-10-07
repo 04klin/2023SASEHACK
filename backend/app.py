6cf90e803beace7f8a876446e6ae88df5f5e6997
Albert Seo <amseo@ncsu.edu>
2023-10-07 14:54:27 -0400

    started backend stuff

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt #pip install Flask-Bcrypt = https://pypi.org/project/Flask-Bcrypt/
 from flask_cors import CORS, cross_origin #ModuleNotFoundError: No module named 'flask_cors' = pip install Flask-Cors
-from models import db, User
+from model import db, User
  
 app = Flask(__name__)
  
